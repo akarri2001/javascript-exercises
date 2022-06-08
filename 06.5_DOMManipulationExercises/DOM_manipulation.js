8aa85895f75087abf3007a5bb6419e01eaf8be2e
Akash Karri <akarri2001@gmail.com>
2022-06-08 19:04:44 -0400

    Complete exercise 1

---
+++ 06.5_DOMManipulationExercises/DOM_manipulation.js
@@ -0,0 +1,10 @@
+
+const container = document.querySelector('#container');
+
+const exercise1 = document.createElement('p');
+exercise1.classList.add('exercise1');
+exercise1.textContent = "Hey I'm red!";
+exercise1.style['color'] = "red";
+container.appendChild(exercise1);
+
+

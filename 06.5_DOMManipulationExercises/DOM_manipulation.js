81f515ae560851dc06baadf4b3da2612d6ba52c9
Akash Karri <akarri2001@gmail.com>
2022-06-08 19:09:00 -0400

    Complete div container for exercise 3

--- 06.5_DOMManipulationExercises/DOM_manipulation.js
+++ 06.5_DOMManipulationExercises/DOM_manipulation.js
@@ -12,3 +12,12 @@ const exercise2 = document.createElement('h3');
 exercise2.textContent = "I'm a blue h3!";
 exercise2.style['color'] = 'blue';
 container.appendChild(exercise2);
+
+//exercise 3 
+
+const exercise3 = document.createElement('div');
+exercise3.classList.add('exercise3');
+exercise3.style['border'] = "3px solid black";
+exercise3.style['background-color'] = 'pink';
+exercise3.textContent = "test"
+container.appendChild(exercise3);

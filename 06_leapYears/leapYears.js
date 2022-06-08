e82ca256bffb1c7433bfbcd1fa3ebadf4a2fc831
Akash Karri <akarri2001@gmail.com>
2022-06-08 03:20:22 -0400

    Complete leapYears challenge

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
 
+    if (inputYear%4 ===0 && (inputYear%100 !=0 || inputYear%400 ==0)){
+        return true;
+    }
+    return false;
 };
+
+console.log(leapYears(1984))
+console.log(leapYears(1900))
+console.log(leapYears(2000))
+console.log(leapYears(2004))
+
 
 // Do not edit below this line
 module.exports = leapYears;

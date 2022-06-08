a2d57babe5d5f08a8803165ac95f098578a50d46
Akash Karri <akarri2001@gmail.com>
2022-06-08 03:14:24 -0400

    Solve sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
 const sumAll = function() {
 
+    for (const arg of arguments){
+        if (typeof(arg) != 'number' || arg < 0){
+            return 'ERROR';
+        }
+    }
+
+    let smallerNumber = Math.min.apply(Math, arguments);
+    let largerNumber = Math.max.apply(Math, arguments);
+    let sumVal = 0;
+    for(let i =smallerNumber; i<largerNumber+1; i++){
+        sumVal += i;
+    }
+    return sumVal;
 };
+
+// console.log(sumAll(1, 4));
+// console.log(sumAll(1, 4000));
+// console.log(sumAll(123, 1));
+// console.log(sumAll(-10, 4));
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
